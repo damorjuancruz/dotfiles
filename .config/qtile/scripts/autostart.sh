c272871271f0177b9700a930344624902246605c
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2021-05-19 09:50:47 -0300

    Set second screen to always be on the left. Disabled mouse acceleration

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+xrandr --output DVI-D-1 --left-of HDMI-0 &
+xinput --set-prop 21 'libinput Accel Profile Enabled' 0, 1 &
 picom --experimental-backends &
 volumeicon &
 nm-applet --indicator &
