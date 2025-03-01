b2166295d1441cae99386ed18d953f2ccd785d07
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2025-03-01 14:39:59 -0300

    init ags

--- hm-modules/ags.nix
+++ hm-modules/ags.nix
@@ -7,13 +7,13 @@
     enable = true;
 
     # null or path, leave as null if you don't want hm to manage the config
-    # configDir = ../ags;
+    configDir = ./sources/ags;
 
     # additional packages to add to gjs's runtime
     extraPackages = with pkgs; [
-      gtksourceview
-      webkitgtk
-      accountsservice
+      # gtksourceview
+      # webkitgtk
+      # accountsservice
     ];
   };
 }
