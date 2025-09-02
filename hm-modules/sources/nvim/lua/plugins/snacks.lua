07ae120e920ed1a2e552462fefd438572b8de277
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2025-09-02 08:40:34 -0300

    mini-surround, disable scroll animation, centered scroll, customize dashboard

---
+++ hm-modules/sources/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,36 @@
+return {
+  "snacks.nvim",
+  opts = {
+    scroll = { enabled = false },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 2,
+        },
+        { section = "startup" },
+      },
+      preset = {
+        header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+      }
+    }
+  },
+}
