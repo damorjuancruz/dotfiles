8e9238c691f95eae95d1edd95a125b258b1c57c9
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2025-11-23 23:40:03 -0300

    update system and fix prisma in node-24 devshell

--- devEnvs/node-24.nix
+++ devEnvs/node-24.nix
@@ -5,7 +5,14 @@ pkgs.mkShell {
     nodejs_24
     pnpm
     yarn
+    openssl
   ];
 
-  shellHook = "exec fish";
+  shellHook = with pkgs; ''
+    export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+    exec fish
+  '';
 }
