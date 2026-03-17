960984ac425cceb61ec73dc9e3a8ec0700ccf65a
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2026-03-16 21:09:17 -0300

    [move to CachyOS + Niri] ghostty

--- hm-modules/ghostty.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, ... }:
-{
-  home.packages = with pkgs; [
-    ghostty
-    nerd-fonts.jetbrains-mono
-  ];
-
-  xdg.configFile.ghostty.source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles-path}/hm-modules/sources/ghostty";
-}
