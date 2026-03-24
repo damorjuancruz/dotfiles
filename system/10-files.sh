9abaf48c8d733524983e3474d3530966bd1ae7cd
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2026-03-24 15:10:26 -0300

    [move to CachyOS + Niri] faillock

--- system/10-files.sh
+++ system/10-files.sh
@@ -4,7 +4,11 @@ source ~/dotfiles/system/00-helpers.sh
 
 white_list=(
   'etc/pacman.conf'
+  'etc/security/faillock.conf'
+  'etc/pam.d/system-auth'
 )
 IgnorePathsExcept "/" "${white_list[@]}"
 
 CopyFile /etc/pacman.conf
+CopyFile /etc/pam.d/system-auth
+CopyFile /etc/security/faillock.conf
