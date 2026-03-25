1ea75b8e3ac191cc143eb6effcbea95d916a1a4d
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2026-03-25 00:31:15 -0300

    [move to CachyOS + Niri] fix polkit and install noctalia polkit agent plugin

--- system/10-files.sh
+++ system/10-files.sh
@@ -6,9 +6,11 @@ white_list=(
   'etc/pacman.conf'
   'etc/security/faillock.conf'
   'etc/pam.d/system-auth'
+  'etc/systemd/system/polkit-agent-helper@.service.d/override.conf'
 )
 IgnorePathsExcept "/" "${white_list[@]}"
 
 CopyFile /etc/pacman.conf
-CopyFile /etc/pam.d/system-auth
 CopyFile /etc/security/faillock.conf
+CopyFile /etc/pam.d/system-auth
+CopyFile /etc/systemd/system/polkit-agent-helper@.service.d/override.conf
